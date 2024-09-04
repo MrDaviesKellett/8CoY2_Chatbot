01314c0211bc95a53e4e37038e7569109c6f43f2
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-04 04:38:09 +0000

    Wed, Sep 4, 2024, 4:38 AM +00:00

--- lesson1.py
+++ lesson1.py
@@ -5,7 +5,7 @@ print("hello world")
 # we assign a string "Mr Davies" to a variable
 name = "Mr Davies"
 
-
+# umm so basically, the input the user types to the question "what is your name" will be assigned to the name variable.
 name = input("what is your name? ")
 
 # if statement, if the condition is true, then do the code below
