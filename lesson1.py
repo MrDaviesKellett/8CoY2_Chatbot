2494b738e3a584ba5265d9f96f22689493a24623
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-04 04:10:32 +0000

    Wed, Sep 4, 2024, 4:10 AM +00:00

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,7 @@
-# print displays text in the console
-# speechmarks around words means string
+# print statement displays text in the console
+# speechmarks around words means string (string of characters)
 print("hello world")
+
 
 name = "Mr Davies"
 if(name == "Mr Davies"):
