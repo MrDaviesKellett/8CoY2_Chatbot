d9a43e55ab21d01d354d15005409d03c025a0139
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-04 04:12:25 +0000

    Wed, Sep 4, 2024, 4:12 AM +00:00

--- lesson1.py
+++ lesson1.py
@@ -2,7 +2,7 @@
 # speechmarks around words means string (string of characters)
 print("hello world")
 
-
+# we assign a string "Mr Davies" to a variable
 name = "Mr Davies"
 if(name == "Mr Davies"):
     print("no not you!?!")
