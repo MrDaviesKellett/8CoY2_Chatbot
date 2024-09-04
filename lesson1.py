764e19173109cf8160fb0fae4d69bede6e5fa5a4
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-04 04:33:16 +0000

    Wed, Sep 4, 2024, 4:33 AM +00:00

--- lesson1.py
+++ lesson1.py
@@ -5,7 +5,7 @@ print("hello world")
 # we assign a string "Mr Davies" to a variable
 name = "Mr Davies"
 
-input("what is your name? ")
+name = input("what is your name? ")
 
 # if statement, if the condition is true, then do the code below
 # double equals check for equality
