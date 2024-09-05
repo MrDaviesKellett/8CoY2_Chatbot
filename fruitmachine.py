99771df5e2554e5852ef62abe29a5f342103df6c
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-05 01:38:10 +0000

    Thu, Sep 5, 2024, 1:38 AM +00:00

--- fruitmachine.py
+++ fruitmachine.py
@@ -8,7 +8,7 @@ symbols = ['Cherry','Lemon','Bell','Skull','Orange','Star']
 dollars = 1
 cents = 0
 
-balance = 1.0 # don't type this
+play = input("do you want to play? ")
 
 # three rollers
 roller1 = choice(symbols)
