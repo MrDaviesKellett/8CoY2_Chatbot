0d3d21e0767c02d3f7cdd8fd646213774e339f95
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-05 01:37:06 +0000

    Thu, Sep 5, 2024, 1:37 AM +00:00

--- fruitmachine.py
+++ fruitmachine.py
@@ -4,6 +4,12 @@ from random import choice
 # symbols
 symbols = ['Cherry','Lemon','Bell','Skull','Orange','Star']
 
+# money
+dollars = 1
+cents = 0
+
+balance = 1.0 # don't type this
+
 # three rollers
 roller1 = choice(symbols)
 roller2 = choice(symbols)
