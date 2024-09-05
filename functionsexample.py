08c0834fd95f7700a844500ed14207d85ade7bce
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-05 01:26:38 +0000

    Thu, Sep 5, 2024, 1:26 AM +00:00

---
+++ functionsexample.py
@@ -0,0 +1,8 @@
+# functions are a way we can group code together
+# we need to 'define' functions using def
+def multiply(num1, num2):
+    return num1 * num2
+
+print( multiply(1000000,1000000009) )
+print( multiply(582423,12803701872) )
+print( multiply(19238021374967,71837) )
