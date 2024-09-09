c4e89329ddd789b3498d733136a3e288b78e10ae
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-09 06:39:36 +0000

    Mon, Sep 9, 2024, 6:39 AM +00:00

--- lesson1.py
+++ lesson1.py
@@ -10,5 +10,9 @@ name = input("what is your name? ")
 
 # if statement, if the condition is true, then do the code below
 # double equals check for equality
-if(name == "Mr Davies"):
+if name == "Mr Davies":
     print("no not you!?!")
+else: # if anything else do this!
+    print("welcome", name) # two parameters (two things in the brackets!)
+    print("welcome " + name) # concatenation, joining together two strings, using the + symbol
+    print(f"Welcome {name}") # format strings, use of f before a string, then use of curly brackets inside the string to put variables or math or other things...
