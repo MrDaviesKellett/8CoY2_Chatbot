a96df2bf0242052e85ee3687bcee4f0d2874ed8b
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-09 04:35:38 +0000

    Mon, Sep 9, 2024, 4:35 AM +00:00

--- pygameExample.py
+++ pygameExample.py
@@ -1,1 +1,19 @@
-from pygame import 
+import pygame
+pygame.init() # initalise
+
+screen = pygame.display.set_mode((1280,720)) #creates a surface
+clock = pygame.time.Clock()
+
+while True: # game loop
+    # Process player inputs.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    screen.fill((255,255,255))  # Fill the display with a solid color
+    myRect = pygame.Rect(20,20,400,300)
+    pygame.draw.rect(screen,(255,0,0),myRect)
+
+    pygame.display.flip()  # Refresh on-screen display
+    clock.tick(60)         # wait until next frame (at 60 FPS)
