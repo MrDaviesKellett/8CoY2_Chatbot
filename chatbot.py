9f784a916d36c269fa8feb6d3b917782e2fd6d81
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-11 04:05:07 +0000

    Wed, Sep 11, 2024, 4:05 AM +00:00

--- chatbot.py
+++ chatbot.py
@@ -4,5 +4,6 @@ name = input("What should I call you? ")
 chatbotName = input("Who am I? ")
 print(f"I am {chatbotName}")
 
-for _ in range(3):
-    
+# _ means I don't care
+for i in range(3):
+    print(i)
