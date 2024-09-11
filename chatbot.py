714a002d92ea38ee9a07d088726a4e049e66850e
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2024-09-11 04:14:14 +0000

    Wed, Sep 11, 2024, 4:14 AM +00:00

--- chatbot.py
+++ chatbot.py
@@ -8,4 +8,6 @@ print(f"I am {chatbotName}")
 # _ means I don't care
 for i in range(3): # do something 3 times
     # print(i) # 0, 1, 2
-    print()
+
+    print(f"I'm asking for the {i + 1} time are you sure your name is {name}?")
+    ans = input("")
